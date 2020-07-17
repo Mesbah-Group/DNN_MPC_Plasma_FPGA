314e0ec7467d66097059a5f46ec24fde5c2fe534
kchan45 <55125954+kchan45@users.noreply.github.com>
2020-07-17 16:11:17 -0700

    update tcl files for no optimizations

--- hls_src/solution1_no_optm/directives.tcl
+++ hls_src/solution1_no_optm/directives.tcl
@@ -3,6 +3,3 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
 ############################################################
-set_directive_interface -mode s_axilite "nn_accel"
-set_directive_interface -mode axis -register -register_mode both "nn_accel" INPUT_STREAM
-set_directive_interface -mode axis -register -register_mode both "nn_accel" OUTPUT_STREAM
--- hls_src/solution1_no_optm/script.tcl
+++ hls_src/solution1_no_optm/script.tcl
@@ -12,12 +12,12 @@ add_files -tb neural_net/solution1_no_optm/neural_net_test.cpp -cflags "-Wno-unk
 open_solution "solution1_no_optm"
 set_part {xc7z020-clg400-1}
 create_clock -period 10 -name default
-config_export -format ip_catalog -rtl verilog -vivado_optimization_level 0 -vivado_phys_opt none -vivado_report_level 0
-config_schedule -effort medium -enable_dsp_full_reg -relax_ii_for_timing -verbose=0
-config_compile -name_max_length 80 -no_signed_zeros=0 -pipeline_loops 64 -unsafe_math_optimizations=0
-config_interface -clock_enable=0 -expose_global=0 -m_axi_addr64 -m_axi_offset off -register_io off -trim_dangling_port=0
-config_rtl -encoding onehot -kernel_profile=0 -module_auto_prefix -mult_keep_attribute=0 -reset control -reset_async=0 -reset_level high -verbose=0
-set_clock_uncertainty 27%
+config_schedule -effort medium -enable_dsp_full_reg=0 -relax_ii_for_timing -verbose=0
+config_compile -name_max_length 20 -no_signed_zeros=0 -pipeline_loops 0 -unsafe_math_optimizations=0
+config_interface -clock_enable=0 -expose_global=0 -m_axi_addr64=0 -m_axi_offset off -register_io off -trim_dangling_port=0
+config_rtl -encoding onehot -kernel_profile=0 -module_auto_prefix=0 -mult_keep_attribute=0 -reset control -reset_async=0 -reset_level high -verbose=0
+config_export -format ip_catalog -rtl verilog -vivado_optimization_level 2 -vivado_phys_opt place -vivado_report_level 0
+config_sdx -optimization_level none -target none
 source "./neural_net/solution1_no_optm/directives.tcl"
 csim_design
 csynth_design
